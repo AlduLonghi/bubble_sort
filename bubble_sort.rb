117b05ae7950fc64db5d61cdde01e6ba4819367b
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-07-15 14:10:49 -0300

    fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,37 +1,33 @@
+# frozen_string_literal: true
+
+# bubble_sort
 
 def bubble_sort(arr)
   arr.length.times do
     i = 0
     while i < arr.length - 1
-      if arr[i] > arr [i + 1]
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      else
-        arr = arr
-      end
+      arr[i] > arr [i + 1] ? (arr[i], arr[i + 1] = arr[i + 1], arr[i]) : (arr = arr)
       i += 1
     end
   end
   print "#{arr}\n"
 end
 
- bubble_sort([2, 7, 5, 1, 10, 5, 39, 20, 6, 8, 17])
+bubble_sort([2, 7, 5, 1, 10, 5, 39, 20, 6, 8, 17])
 
+# bubble_sort_by (accepting a block as a parameter)
 
 def bubble_sort_by(arr)
   arr.length.times do
     i = 0
     while i < arr.length - 1 
-      if yield(arr[i], arr[i + 1]).positive?
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      else
-        arr = arr
-      end
+      yield(arr[i], arr[i + 1]).positive? ? (arr[i], arr[i + 1] = arr[i + 1], arr[i]) : (arr = arr)
       i += 1
     end
   end
   print "#{arr}\n"
 end
 
-bubble_sort_by([2, 7, 5, 6, 1, 67, 34]) do |left, right|
+bubble_sort_by([2, 7, 5, 6, 1]) do |left, right|
   left - right
 end
