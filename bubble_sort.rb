2fca320a9f365ab415cb4864c6cda3d5e9541f4e
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-07-15 16:11:15 -0300

    fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -6,28 +6,13 @@ def bubble_sort(arr)
   arr.length.times do
     i = 0
     while i < arr.length - 1
-      arr[i] > arr [i + 1] ? (arr[i], arr[i + 1] = arr[i + 1], arr[i]) : (arr = arr)
+      if arr[i] > arr [i + 1]
+        (arr[i], arr[i + 1] = arr[i + 1], arr[i])
+      else arr = arr
+      end
       i += 1
     end
   end
-  print "#{arr}\n"
 end
 
-bubble_sort([2, 7, 5, 1, 10, 5, 39, 20, 6, 8, 17])
-
-# bubble_sort_by (accepting a block as a parameter)
-
-def bubble_sort_by(arr)
-  arr.length.times do
-    i = 0
-    while i < arr.length - 1 
-      yield(arr[i], arr[i + 1]).positive? ? (arr[i], arr[i + 1] = arr[i + 1], arr[i]) : (arr = arr)
-      i += 1
-    end
-  end
-  print "#{arr}\n"
-end
-
-bubble_sort_by([2, 7, 5, 6, 1]) do |left, right|
-  left - right
-end
+p bubble_sort([2, 7, 5, 1, 10, 5, 39, 20, 6, 8, 17])
---
+++ bubble_sort_by.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# bubble_sort_by (accepting a block as a parameter)
+
+def bubble_sort_by(arr)
+  arr.length.times do
+    i = 0
+    while i < arr.length - 1
+      yield(arr[i], arr[i + 1]).positive? ? (arr[i], arr[i + 1] = arr[i + 1], arr[i]) : (arr = arr)
+      i += 1
+    end
+  end
+  puts arr
+end
+
+bubble_sort_by([2, 7, 5, 6, 1]) do |left, right|
+  left - right
+end
