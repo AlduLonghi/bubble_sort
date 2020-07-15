9f2821df1d4ea0953100e20130a627dc95bf7759
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-07-15 13:01:27 -0300

    fix linter errors

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # bubble_sort
+
+This project consists of creating an method which takes an Array as a parameter and returns it sorted in ascending order.
+
+In this project:
+
+1. We built the #bubble_sort method.
+2. We built the #bubble_sort_by method which takes a block as a parameter.
+
+## Built with 
+
+-Ruby
+
+## Getting Started
+
+To get a local copy of the repository please run the following commands on your terminal:
+
+$ cd <folder>
+$ git clone https://github.com/AlduLonghi/bubble_sort.git
+
+## Author
+
+👤 **Aldana**
+​
+
+- Github: [@AlduLonghi](https://github.com/AlduLonghi)
+
+- Twitter: [@AbrilLonghi](https://twitter.com/AbrilLonghi)
+
+- Linkedin: [Aldana Abril Longhi](https://www.linkedin.com/in/aldana-abril-longhi-a842ba1a7/)
+
+👤 **Yigit**
+
+- Github: [@yigitm](https://github.com/yigitm)
+
+- Twitter: [@yigitmersin](https://twitter.com/yigitmersin)
+
+- Linkedin: [ygtmrsn](https://www.linkedin.com/in/yigitmersin)
+
+## �� Contributing
+
+- To contribute to this project:
+- Fork this repository & clone locally.
+- Create an upstream remote and sync your local copy.
+- create a new branch.
+- Push your code to your origin repository.
+- Create a new Pull Request .
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+​
+
+## Acknowledgments
+
+- The Odin Project
+- Microverse curriculum.
+
--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,39 +1,37 @@
-=begin 
-We have to build a method that takes an array as a parameter. 
-Inside the method we have to loop through the array and compare two of its elements to see which one is bigger. 
-If the one on the left is bigger than the one on the right, the elements swap. If not they stay the same. 
-Repeat this until we have the entire array sorted from the smallest to the biggest number.
+
 def bubble_sort(arr)
-    arr.length.times {
+  arr.length.times do
     i = 0
     while i < arr.length - 1
-      if arr[i] > arr [i+1]
-        arr[i], arr[i+1] = arr[i+1], arr[i]
+      if arr[i] > arr [i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
       else
         arr = arr
       end
-      i+= 1
-    end
-}
-   print arr
-end
- bubble_sort([2, 7, 5, 1, 10, 5, 39, 20, 6, 8, 17])
-=end
-
- def bubble_sort_by(arr)
-    arr.length.times {
-      i = 0
-      while i < arr.length - 1 
-        if yield > 0 
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-        else
-          arr = arr
-        end
       i += 1
-      end
-    }
+    end
+  end
+  print "#{arr}\n"
 end
 
-bubble_sort_by([2, 7, 5]) do |left,right|
-    left - right
+ bubble_sort([2, 7, 5, 1, 10, 5, 39, 20, 6, 8, 17])
+
+
+def bubble_sort_by(arr)
+  arr.length.times do
+    i = 0
+    while i < arr.length - 1 
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      else
+        arr = arr
+      end
+      i += 1
+    end
+  end
+  print "#{arr}\n"
+end
+
+bubble_sort_by([2, 7, 5, 6, 1, 67, 34]) do |left, right|
+  left - right
 end
